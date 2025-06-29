ddc74c951510515ef0474bf415707707d0f94670
Skibidi sigma <Tan_yeong_ern_brighton@s2024.ssts.edu.sg>
2025-06-29 15:11:41 +0800

    Fix footer CSS and JSX syntax errors

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -12,9 +12,9 @@ const Footer = () => {
                     <img src={assets.logo} alt="" />
                     <p>smthsmthsmthsmthsmthsmthstmhsmthsmthsmthsmthsmthsmthstmhsmthsmhsmthsmthsmthsmthstmh</p>
                     <div className="footer-social-icons">
-                        <img src={assets.facebook_icon}alt="" />
-                        <img src={assets.twitter_icon}alt="" />
-                        <img src={assets.linkedin_icon}alt="" />
+                        <img src={assets.facebook_icon} alt="" />
+                        <img src={assets.twitter_icon} alt="" />
+                        <img src={assets.linkedin_icon} alt="" />
                     </div>
                 </div>
                 <div className="footer-content-center">
